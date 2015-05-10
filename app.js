0ae95e50b2a7c59aadd95f711fc7e9815c3adbaf
admin-qqq <jixevazeva@ubismail.net>
2015-05-10 20:29:22 +0300

    add config and auth

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 var express = require('express');
+var basicAuth = require('./module/auth');
 var app = express();
+app.disable('x-powered-by');
 
-app.get('/', function (req, res) {
+app.get('/ddd', basicAuth,function (req, res) {
     res.send('Hello World!');
 });
 
+app.use(function(req, res, next) {
+    res.redirect(301, 'https://google.com');
+});
 var server = app.listen(3000, function () {
-
     var host = server.address().address;
     var port = server.address().port;
-
     console.log('Example app listening at http://%s:%s', host, port);
-
 });
